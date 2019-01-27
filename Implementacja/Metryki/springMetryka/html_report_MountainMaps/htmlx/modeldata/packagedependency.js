74bfe1e32c79e078b18e4ebe5afd7ac561d924a5
mateuszp <mpftp10@gmail.com>
2019-01-27 15:16:07 +0100

    dodano metryke

---
+++ Implementacja/Metryki/springMetryka/html_report_MountainMaps/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,26 @@
+var matrix = [[0,0,0,0,2,0,0,0],[1,0,0,2,0,3,1,0],[0,0,0,0,0,0,0,0],[0,3,0,0,0,0,2,0],[1,0,0,0,0,1,0,0],[3,0,0,0,2,0,0,0],[0,0,0,0,0,0,0,2],[0,0,0,0,0,0,0,0]];
+var packages = [{
+"name": " MountainMaps.Section", "color": " #3182bd"
+}
+,{
+"name": " MountainMaps.Hiker", "color": " #6baed6"
+}
+,{
+"name": " MountainMaps", "color": " #9ecae1"
+}
+,{
+"name": " MountainMaps.HikerBadge", "color": " #c6dbef"
+}
+,{
+"name": " MountainMaps.Point", "color": " #e6550d"
+}
+,{
+"name": " MountainMaps.HikingTrail", "color": " #fd8d3c"
+}
+,{
+"name": " MountainMaps.Badge", "color": " #fdae6b"
+}
+,{
+"name": " MountainMaps.Utils", "color": " #fdd0a2"
+}
+];
